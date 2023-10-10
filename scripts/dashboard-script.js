66d1d62b1247410bc1a21281d13415710d8d6d8d
Ramya <ramyalathasrikarri@gmail.com>
2023-10-10 14:32:50 +1030

    dashboard-dropdown

--- scripts/dashboard-script.js
+++ scripts/dashboard-script.js
@@ -12,9 +12,10 @@ function getProjectDetail(elem) {
 
     const projectKey = tdElements[0].textContent;
     const projectName = tdElements[1].textContent;
-    
-    return {projectKey, projectName};
+
+    return { projectKey, projectName };
 }
+
 // Adding event listeners to the links
 document.getElementById('create-project').addEventListener('click', function () {
     navigateToPage('./project/create-project.html');
@@ -27,5 +28,24 @@ document.getElementById('kanban1').addEventListener('click', function () {
 
 document.getElementById('kanban2').addEventListener('click', function () {
     const { projectKey, projectName } = getProjectDetail('kanban2');
-    navigateToPage(`./project/kanban.html?projectKey=${encodeURIComponent(projectKey)}&projectName=${encodeURIComponent(projectName)}`);
+    navigateToPage(`./project/kanban2.html?projectKey=${encodeURIComponent(projectKey)}&projectName=${encodeURIComponent(projectName)}`);
+});
+
+// Added code for dropdown menu
+document.addEventListener("DOMContentLoaded", function () {
+    const dropdownIcon = document.getElementById("dropdownIcon");
+    const dropdownContent = document.getElementById("dropdownContent");
+
+    dropdownIcon.addEventListener("click", function () {
+        dropdownContent.classList.toggle("show");
+    });
+
+    // Close the dropdown when clicking outside of it
+    window.addEventListener("click", function (event) {
+        if (!event.target.matches("#dropdownIcon")) {
+            if (dropdownContent.classList.contains("show")) {
+                dropdownContent.classList.remove("show");
+            }
+        }
+    });
 });
