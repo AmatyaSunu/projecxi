77445cf550897bdfb8a8b978cc76deccd9f6c01b
AmatyaSunu <sunidhiamatya@gmail.com>
2023-10-12 08:36:01 +1030

    commented

--- Project/kanband.php
+++ Project/kanband.php
@@ -14,9 +14,9 @@
 
 <body>
     <?php
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
+    // ini_set('display_errors', 1);
+    // ini_set('display_startup_errors', 1);
+    // error_reporting(E_ALL);
 
     session_start();
     if (isset($_SESSION['projectId'])) {
--- confirmation/new-ticket-confirmation.php
+++ confirmation/new-ticket-confirmation.php
@@ -50,9 +50,9 @@
 
     <!-- PHP code to handle server -->
     <?php
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
+    // ini_set('display_errors', 1);
+    // ini_set('display_startup_errors', 1);
+    // error_reporting(E_ALL);
 
     session_start();
     echo "Project ID: " . $_SESSION['projectId'] . "<br>";
