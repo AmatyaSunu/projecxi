32083be10cfe2a4347bcd5fd6441384e8a9ea66b
AmatyaSunu <sunidhiamatya@gmail.com>
2023-10-17 08:45:58 +1030

    refactoring of code

--- dashboard.php
+++ dashboard.php
@@ -16,11 +16,10 @@
 
 <body>
     <?php
-
     //uncomment for debugging
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
+    // ini_set('display_errors', 1);
+    // ini_set('display_startup_errors', 1);
+    // error_reporting(E_ALL);
 
     require_once "inc/dbconn.inc.php";
     ?>
